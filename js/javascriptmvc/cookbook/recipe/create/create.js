061209ce3d8066feb33b631bb67f9040eb414e58
Elnur Aghayev <agayeve@sfreserves.org>
2012-11-16 16:58:02 -1000

    Modify the fixtures

---
+++ js/javascriptmvc/cookbook/recipe/create/create.js
@@ -0,0 +1,32 @@
+steal( 'jquery/controller',
+       'jquery/view/ejs',
+	   'jquery/dom/form_params',
+	   'jquery/controller/view',
+	   'cookbook/models' )
+	.then('./views/init.ejs', function($){
+
+/**
+ * @class Cookbook.Recipe.Create
+ * @parent index
+ * @inherits jQuery.Controller
+ * Creates recipes
+ */
+$.Controller('Cookbook.Recipe.Create',
+$('form#create').cookbook_recipe_create()
+/** @Prototype */
+{
+	init : function(){
+		this.element.html(this.view());
+	},
+	submit : function(el, ev){
+		ev.preventDefault();
+		this.element.find('[type=submit]').val('Creating...')
+		new Cookbook.Models.Recipe(el.formParams()).save(this.callback('saved'));
+	},
+	saved : function(){
+		this.element.find('[type=submit]').val('Create');
+		this.element[0].reset()
+	}
+})
+
+});
